9d27b72a98b71f07eb9c94377fc991283a68dcd8
arthur3523 <arthurmendanha1410@gmail.com>
2026-08-03 11:50:52 -0300

    feat: primeira versão modular + documentação inicial

---
+++ src/utils/utilis.js
@@ -0,0 +1,27 @@
+function ask(question) {
+  return rl.question(question);
+}
+
+async function pause() {
+  await ask('\nPressione ENTER para continuar...');
+}
+import readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+export function ask(question) {
+  return new Promise((resolve) => {
+    rl.question(question, resolve);
+  });
+}
+
+export async function pause() {
+  await ask('\nPressione ENTER para continuar...');
+}
+
+export function closeInput() {
+  rl.close();
+}
